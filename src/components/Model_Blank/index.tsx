381ae7338ff2ebb1cc6f2c2ae1cedfd258d81226
Eduardo Eberhardt <eduardoeberhardt@gmail.com>
2020-08-23 16:33:16 -0300

    create blank start component with store connectin

---
+++ src/components/Model_Blank/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Dispatch } from 'redux';
+import {connect} from 'react-redux';
+import * as actions from '../store/actions';
+import {reducers} from "../store/reducers";
+
+import styles from './styles';
+
+interface StateProps{
+    data:reducers,
+    removeTodo:(dispatch:{})=>void
+  }
+
+const List = ({removeTodo, data}:StateProps) => { 
+  return <View />;
+
+  
+}
+
+const mapStateToProps = (state:reducers) =>({
+    data:state
+  });
+  
+  const mapDispatchToProps = (dispatch:Dispatch) =>({
+    removeTodo: (todo:any) => dispatch(actions.addTodo(todo))
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
